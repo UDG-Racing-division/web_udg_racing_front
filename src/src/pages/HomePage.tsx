a5dae512f05cb0b6bf25bd6f97ad7c4cecbcee63
JC <52259156+jc-discdev@users.noreply.github.com>
2026-06-22 18:28:15 +0200

    fix routes

--- src/src/pages/HomePage.tsx
+++ src/src/pages/HomePage.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Navigation } from '../components/Navigation';
-import { Hero } from '../components/Hero';
-import { StatsSection } from '../components/StatsSection';
-import { AboutSection } from '../components/AboutSection';
-import { TeamSection } from '../components/TeamSection';
-import { NewsSection } from '../components/NewsSection';
-import { SponsorCarousel } from '../components/SponsorCarousel';
-import { Footer } from '../components/Footer';
+import { Navigation } from '../../components/Navigation';
+import { Hero } from '../../components/Hero';
+import { StatsSection } from '../../components/StatsSection';
+import { AboutSection } from '../../components/AboutSection';
+import { TeamSection } from '../../components/TeamSection';
+import { NewsSection } from '../../components/NewsSection';
+import { SponsorCarousel } from '../../components/SponsorCarousel';
+import { Footer } from '../../components/Footer';
 export function HomePage() {
   return <div className="w-full min-h-screen bg-black">
     <Navigation />
     <Hero />
-    <StatsSection />
+    <StatsSection stats={[]} />
     <AboutSection />
     <TeamSection />
-    <SponsorCarousel />
-    <NewsSection />
+    <SponsorCarousel sponsors={[]} />
+    <NewsSection news={[]} />
     <Footer />
   </div>;
 }
